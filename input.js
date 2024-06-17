105aa81f8a2b599b6d048a6e494a094ef25988c0
FarflungEyestrain <stedgegavin@gmail.com>
2024-06-17 10:08:49 -0400

    chgchghcg

--- input.js
+++ input.js
@@ -3,13 +3,14 @@ let messages = document.querySelector(".messages");
 
 // CODE ALONG, PART 1 CONTINUED
 // 3. Declare a variable to store the input element
-
+let commentInput= document.querySelector(".user-input");
 
 submitButton.addEventListener("click", function() {
     // 4. Declare a variable to store the value of the input
     // 5. Use the console to check a value is saved.
     //  - Type something and click the button to confirm it works!
-
+    let userComment = commentInput.value;
+    console.log(userComment);
 
     
 
